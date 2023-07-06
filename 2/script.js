d4c35090fda5fd2ccb09d9ceb26e20164be215e2
Alejandro Grimaldo <alejandrogrim12@gmail.com>
2023-07-06 14:59:14 -0600

    day 1-2

---
+++ 2/script.js
@@ -0,0 +1,10 @@
+const menu = document.querySelector('.menu')
+let lineTop = document.querySelector('.line1')
+let line = document.querySelector('.line2')
+let lineButtom = document.querySelector('.line3')
+
+menu.addEventListener('click', (e) => {
+  lineTop.classList.toggle('top')
+  line.classList.toggle('centro')
+  lineButtom.classList.toggle('buttom')
+})
